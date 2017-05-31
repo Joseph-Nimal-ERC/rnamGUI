4c810304604424a4912e8c70a7e2b810460525a4
Joseph-Nimal-ERC <joseph.nimal.fernando@ericsson.com>
2017-05-31 12:33:45 +0530

    RNAM GUI
    Independent GUI application

---
+++ app/scripts/services/projectsService.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('rnaminventoryApp')
+.service('projectsService', ['$http', function($http){
+   this.getProjects = function() {
+      return $http.get('http://localhost:3000/project');
+   };
+
+   this.getProjectById = function(id) {
+      return $http.get('http://localhost:3000/project/getTask/'.concat(id));
+   };
+
+   this.getProjectsUtilization = function() {
+      return $http.get('../projUtil.json');
+   };
+
+   this.saveProject = function(data) {
+      return $http.post('http://localhost:3000/project', data);
+   };
+}]);
