937f3c6bcc1b6e5c1cbdfbcbe9d1177479c167ce
Joseph-Nimal-ERC <joseph.nimal.fernando@ericsson.com>
2017-05-31 12:52:52 +0530

    remove redundant files

--- app/scripts/services/employeeService.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('rnaminventoryApp')
-.service('employeeService', ['$http', function($http){
-   this.getEmployees = function() {
-      return $http.get("./emp.json");
-   };
-}]);
