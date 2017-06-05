384203b006281b2df02354598675b969165aeb9f
Prakash-Britto-ERC <a.jeniffer.prakash.britto@ericsson.com>
2017-06-05 18:01:02 +0530

    Project Gap value update method once the Employee is utilized for the month

--- Server/src/routes/projUtilization.js
+++ Server/src/routes/projUtilization.js
@@ -89,7 +89,7 @@ router.post('/', function(req, res, next) {
 });
 
 /* Update existing Project utilization entry */
-router.put('/', function(req, res, next) {
+router.put('/updateProjectUtilization', function(req, res, next) {
 	for(var key in req.body){
 		var reqBody = req.body[key];
 		ProjUtilization.findByIdAndUpdate({_id : reqBody._id}, reqBody, function(err, projUtilization){
@@ -98,6 +98,58 @@ router.put('/', function(req, res, next) {
 		});
 	}
 	 res.json({message : 'Project Utilization record(s) updated Successfully'});
+});
+
+/* Update project utilization Gap details for the given month and year */
+router.put('/', function(req, res, next) {
+  var projectId = req.query.projectId;
+  var year = req.query.year;
+  var month = req.query.month;
+  var mongoose = require('mongoose'); 
+  ProjUtilization.find({projId : mongoose.Types.ObjectId(projectId)} , function (err, projUtilization) {
+    if(projUtilization && projUtilization[0]){
+		for(var i=0; i < projUtilization[0].need_Gap_dtls.length; i++){
+		
+				if(projUtilization[0].need_Gap_dtls[i].year == year){
+					if(month == 'Jan'){
+						projUtilization[0].need_Gap_dtls[i].months.Jan_Gap = projUtilization[0].need_Gap_dtls[i].months.Jan_Gap + 1;
+					}
+					else if(month == 'Feb')
+						projUtilization[0].need_Gap_dtls[i].months.Feb_Gap = projUtilization[0].need_Gap_dtls[i].months.Feb_Gap + 1;
+					else if(month == 'Mar')
+						projUtilization[0].need_Gap_dtls[i].months.Mar_Gap = projUtilization[0].need_Gap_dtls[i].months.Mar_Gap + 1;
+					else if(month == 'Apr')
+						projUtilization[0].need_Gap_dtls[i].months.Apr_Gap = projUtilization[0].need_Gap_dtls[i].months.Apr_Gap + 1;
+					else if(month == 'May')
+						projUtilization[0].need_Gap_dtls[i].months.May_Gap = projUtilization[0].need_Gap_dtls[i].months.May_Gap + 1;
+					else if(month == 'Jun')
+						projUtilization[0].need_Gap_dtls[i].months.Jun_Gap = projUtilization[0].need_Gap_dtls[i].months.Jun_Gap + 1;
+					else if(month == 'Jul')
+						projUtilization[0].need_Gap_dtls[i].months.Jul_Gap = projUtilization[0].need_Gap_dtls[i].months.Jul_Gap + 1;
+					else if(month == 'Aug')
+						projUtilization[0].need_Gap_dtls[i].months.Aug_Gap = projUtilization[0].need_Gap_dtls[i].months.Aug_Gap + 1;
+					else if(month == 'Sep')
+						projUtilization[0].need_Gap_dtls[i].months.Sep_Gap = projUtilization[0].need_Gap_dtls[i].months.Sep_Gap + 1;
+					else if(month == 'Oct')
+						projUtilization[0].need_Gap_dtls[i].months.Oct_Gap = projUtilization[0].need_Gap_dtls[i].months.Oct_Gap + 1;
+					else if(month == 'Nov')
+						projUtilization[0].need_Gap_dtls[i].months.Nov_Gap = projUtilization[0].need_Gap_dtls[i].months.Nov_Gap + 1;
+					else if(month == 'Dec')
+						projUtilization[0].need_Gap_dtls[i].months.Dec_Gap = projUtilization[0].need_Gap_dtls[i].months.Dec_Gap + 1;
+					
+					projUtilization[0].save(function(err, projUtilization){	
+						if(err)
+							return next(err);
+						else
+							res.json(projUtilization);
+					});
+				}	
+		}
+	}
+	else {
+		res.json({message : 'Project Utilization record not found'});
+	}
+  });
 });
 
 /* Delete an Project utilization entry */
