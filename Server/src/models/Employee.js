4e78a4171a4e2c0270f27a990cbd5f8ad83c5234
EAJEBRI <a.jeniffer.prakash.britto@ericsson.com>
2017-06-08 18:56:21 +0530

    Employee and Emp Utilization schema merged

--- Server/src/models/Employee.js
+++ Server/src/models/Employee.js
@@ -2,19 +2,39 @@ var mongoose = require('mongoose');
 var EmployeeScehma = new mongoose.Schema({
   signum: String,
   name: String,
+  personnelNo: String,
+  type: String,
   jobStage: String,
   role: String,
+  businessArea : String,
+  product : String,
   location: String,
   teamName: String,
-  pool: String,
-  expDetails: 
+  egiLineManager : String,
+  pool: String,  
+  mapping_dtls: [ 
   {
-	  Tot_exp : String,
-	  EOC_exp : String,
-	  ECM_exp : String,
-	  HO_month : String
-  }
-  
+                year: Number,
+                projId : {type : Schema.ObjectId, ref : 'projectCollection'},
+                status : String,
+				CU: String,
+                months : 
+                  {
+                                  Jan : Number,
+                                  Feb : Number,
+                                  Mar : Number,
+                                  Apr : Number,
+								  May : Number,
+                                  Jun : Number,
+                                  Jul : Number,
+                                  Aug : Number,
+                                  Sep : Number,
+                                  Oct : Number,
+                                  Nov : Number,
+                                  Dec : Number
+                  }
+  }]
 });
 
 module.exports = mongoose.model('employeeCollection', EmployeeScehma);
+
